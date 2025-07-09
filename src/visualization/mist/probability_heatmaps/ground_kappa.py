753eef56648ffca8f73a397520475db3c4ff7a2a
David Kim <davidkm@berkeley.edu>
2025-07-08 17:12:58 -0700

    kappa fixeds

--- src/visualization/mist/probability_heatmaps/ground_kappa.py
+++ src/visualization/mist/probability_heatmaps/ground_kappa.py
@@ -5,36 +5,30 @@ import qutip as qt
 import scqubits as scq
 import floquet as ft
 
-# system parameters
-# All frequencies in GHz
-g       = 0.120         # coupling strength (GHz)
-EC      = 0.220         # charging energy (GHz)
-omega_r = 5.7           # resonator frequency (GHz)
-omega_d = omega_r - 0.033  # drive frequency (GHz)
+g = 0.120
+EC = 0.220
+omega_r = 5.7
+omega_d = omega_r - 0.033
 
-# sweep parameters
-kappa_vals = np.linspace(0.005, 0.05, 20)  # resonator linewidths κ (GHz)
-nbar_vals  = np.linspace(0, 180, 91)       # photon number grid
-drive_amps = 2.0 * g * np.sqrt(nbar_vals)
+kappa_vals = np.linspace(0.005, 0.05, 20)
+nbar_vals = np.linspace(0, 180, 91)
+drive_amps = 2.0*g*np.sqrt(nbar_vals)
 
-# initialize result matrix
 transition_prob_matrix = np.zeros((len(kappa_vals), len(nbar_vals)))
 
-# fixed detuning (choose a representative value)
-Delta      = 1.0                   # fixed transmon–resonator detuning Δ (GHz)
-omega_q    = omega_r + Delta
-EJ         = (omega_q + EC)**2 / (8 * EC)
+Delta = 1.0
+omega_q = omega_r + Delta
+EJ = (omega_q+EC)**2/(8*EC)
 
-# build transmon Hamiltonians once
-num_states   = 12
+num_states = 12
 qubit_params = dict(EJ=EJ, EC=EC, ng=0.2, ncut=31)
 tmon = scq.Transmon(**qubit_params, truncated_dim=num_states)
-hs   = scq.HilbertSpace([tmon])
+hs = scq.HilbertSpace([tmon])
 hs.generate_lookup()
 
 evals = hs["evals"][0][:num_states]
-H0    = 2 * np.pi * qt.Qobj(np.diag(evals - evals[0]))
-H1    = hs.op_in_dressed_eigenbasis(tmon.n_operator)
+H0 = 2*np.pi*qt.Qobj(np.diag(evals-evals[0]))
+H1 = hs.op_in_dressed_eigenbasis(tmon.n_operator)
 
 options = ft.Options(
     num_cpus=4,
@@ -50,20 +44,18 @@ for i, kappa in enumerate(kappa_vals):
     model = ft.Model(
         H0,
         H1,
-        omega_d_values   = np.array([2 * np.pi * omega_d]),
-        drive_amplitudes = 2 * np.pi * drive_amps
+        omega_d_values=np.array([2*np.pi*omega_d]),
+        drive_amplitudes=2*np.pi*drive_amps
     )
-    fa    = ft.FloquetAnalysis(model, state_indices=list(range(num_states)), options=options)
-    data  = fa.run()
+    fa = ft.FloquetAnalysis(model, state_indices=list(range(num_states)), options=options)
+    data = fa.run()
     floq_modes = data["floquet_modes"][0] 
 
     for j in range(len(nbar_vals)):
-        overlaps = np.abs(floq_modes[j, :, 0])**2
-        ground_branch_idx = int(np.argmax(overlaps))
-        # transition probability out of |g> = 1 - overlap
-        transition_prob_matrix[i, j] = 1 - overlaps[ground_branch_idx]
+        overlaps=np.abs(floq_modes[j, :, 0])**2
+        ground_branch_idx=int(np.argmax(overlaps))
+        transition_prob_matrix[i, j]=1-overlaps[ground_branch_idx]
 
-# plot the heatmap
 plt.figure(figsize=(12, 8))
 im = plt.imshow(
     transition_prob_matrix.T,
