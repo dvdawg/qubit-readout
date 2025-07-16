ad6ed40e976727395e533173062ee4acc40ab26e
David Kim <davidkm@berkeley.edu>
2025-07-16 13:58:55 -0700

    small changes

--- src/visualization/mist/probability_heatmaps/ground_det_resonator.py
+++ src/visualization/mist/probability_heatmaps/ground_det_resonator.py
@@ -7,8 +7,9 @@ import floquet as ft
 
 g = 0.120
 EC = 0.220
-omega_r = 5.7
-omega_d = omega_r - 0.033
+# omega_r = 5.7
+omega_q = 6.289177
+# omega_d = omega_r - 0.033
 
 Delta_vals = np.linspace(0.75, 1.5, 50)
 nbar_vals = np.linspace(5, 30, 26)
@@ -21,8 +22,8 @@ def transmon_levels(n_levels,EC,EJ):
 def quantum_critical_n(omega_qs,omega_r,g):
     return ((omega_qs-omega_r)/(2*g))**2
 
-omega_qs = omega_r + Delta_vals
-n_crit_ana = quantum_critical_n(omega_qs, omega_r, g)
+# omega_qs = omega_r + Delta_vals
+# n_crit_ana = quantum_critical_n(omega_qs, omega_r, g)
 
 options = ft.Options(num_cpus = 8,nsteps = 1000,fit_range_fraction = 1.0,overlap_cutoff = 0.8,floquet_sampling_time_fraction = 0.0,save_floquet_modes = True)
 
@@ -30,7 +31,9 @@ transmon_dim = 6
 osc_dim = int(nbar_vals.max())
 
 for i, Delta in enumerate(Delta_vals):
-    omega_q = omega_r + Delta
+    # omega_q = omega_r + Delta
+    omega_r = omega_q - Delta
+    omega_d = omega_r
     EJ = (omega_q+EC)**2/(8*EC)
     tmon = scq.Transmon(EJ=EJ,EC=EC,ng=0.2,ncut=25,truncated_dim=transmon_dim)
     reson = scq.Oscillator(E_osc=omega_r,truncated_dim=osc_dim)
@@ -51,7 +54,7 @@ plt.figure(figsize=(12,8))
 im = plt.imshow(transition_prob_matrix.T, extent=[Delta_vals[0],Delta_vals[-1],nbar_vals[0],nbar_vals[-1]], aspect='auto', origin='lower', cmap='viridis')
 cbar=plt.colorbar(im)
 cbar.set_label('Transition Probability',fontsize=12)
-plt.plot(Delta_vals,n_crit_ana,'r--',linewidth=2,alpha=0.8,label='JC-like')
+# plt.plot(Delta_vals,n_crit_ana,'r--',linewidth=2,alpha=0.8,label='JC-like')
 plt.xlabel('Transmon–resonator detuning Δ (GHz)',fontsize=12)
 plt.ylabel('Photon number $\\overline{n}$',fontsize=12)
 plt.title('Ground-state leakage probability',fontsize=14)
